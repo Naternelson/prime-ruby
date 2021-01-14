81f3a855eda731fba7229f54d4938cec18159adf
Nate <7nater.nelson@gmail.com>
2021-01-14 16:18:35 -0700

    commit tree 1a4b43ea1504819e50cf53cbe73f6f08861f6156

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(int) #int should be an integer
+    if int < 2 
+        return false
+    end
+    i = 2 #Won't include zero or one cause dividing by those nums is silly ;)
+    iter = 1
+    while i < int
+        puts "Iterated #{iter} times." if int % i == 0
+        return false if int % i == 0 #A Prime number by its own definition, shouldn't have a clean remainder
+        i += 1
+        iter +=1
+    end
+    puts "#{int} is a prime number."
+    puts "Iterated #{iter} times."
+    true
+end
+
+prime?(4)
